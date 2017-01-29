85810b3794240bf4b3537fc2670298d500ed3062
Vinspi <vincent.pierre314@gmail.com>
2017-01-29 17:10:17 +0100

    test intellij ?

--- server/src/ShopManager.java
+++ server/src/ShopManager.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
  * Created by vinspi on 27/01/17.
  */
 public class ShopManager {
+    /* coucou ceci est un test */
     Connection connection = null;
     LinkedList<ItemView> listItem;
 
@@ -20,7 +21,7 @@ public class ShopManager {
         ResultSet resultSet = Manager.getManager().sendRequestQuery(query,connection);
         try {
             while (resultSet.next()){
-               // listItem.add(new PackView(resultSet.getInt("monnaieIRL"),resultSet.getString("monnaieIG"),resultSet.getInt("id_Pack")));
+               //listItem.add(new PackView(resultSet.getInt("monnaieIRL"),resultSet.getString("monnaieIG"),resultSet.getInt("id_Pack")));
             }
         }catch (SQLException e){
             e.printStackTrace();
