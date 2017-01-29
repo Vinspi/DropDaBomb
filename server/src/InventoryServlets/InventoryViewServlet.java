a54e740f10f961827c8b6dc4497e9f18bad112e6
Vinspi <vincent.pierre314@gmail.com>
2017-01-29 17:33:11 +0100

    ajout de tout le projet

---
+++ server/src/InventoryServlets/InventoryViewServlet.java
@@ -0,0 +1,17 @@
+package InventoryServlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by vinspi on 29/01/17.
+ */
+public class InventoryViewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
