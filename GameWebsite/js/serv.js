ace95343a004299928642da717eb0fd5cc27a81a
Vinspi <vincent.pierre314@gmail.com>
2017-03-10 11:41:45 +0100

    ajout de l'architecture img

--- GameWebsite/js/serv.js
+++ GameWebsite/js/serv.js
@@ -20,23 +20,46 @@
   this.cActivesNonRetourneesJ2 = [];
 }*/
 
+var NB_MAX_SALON = 6;
+var NB_MAX_PDV = 200;
+
 var fifoJoueurs = [];
 var listSalons = [];
 
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var express = require("express");
 
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host : '109.7.220.208',
+  host : '217.182.69.175',
   user : 'Vinspi',
   password : 'vinspi13',
   database : 'DropDaBomb',
 });
 
+/* ******************************************************************* */
 
+function initListSalon(){
+  for(var i=0;i<NB_MAX_SALON;i++){
+    listSalons[i] = "LIBRE";
+  }
+}
+
+function insereSalon(room){
+  for(var i=0;i<NB_MAX_SALON;i++){
+    if(listSalons[i] == "LIBRE"){
+      listSalons[i] = room;
+      /* insertion réussie */
+      return true;
+    }
+  }
+  return false;
+}
+
+/* ******************************************************************* */
 
 function etatJoueur(pseudo,deck){
   this.pseudo = pseudo;
@@ -46,6 +69,8 @@ function etatJoueur(pseudo,deck){
   this.deck = [];
   this.cActives = [];
   this.cActivesNonRetournees = [];
+  /* les point du j1 sont MAX_PDV - j2.pdv */
+  this.pdv = 100;
 }
 
 function etatMatch(pseudo1,pseudo2,deck1,deck2) {
@@ -63,16 +88,17 @@ function Room(name, pseudo1, pseudo2){
 
 function fournirSalons(){
   var index = listSalons.length
-  while (fifoJoueurs.length > 2){
+  while (fifoJoueurs.length >= 2){
     var nameRoom = 'room'+index;
     var room = new Room(nameRoom, fifoJoueurs[0].pseudo, fifoJoueurs[1].pseudo);
-    listSalons.push(room);
+    insereSalon(room);
     index++;
     (fifoJoueurs[0].socket).join(nameRoom);
     (fifoJoueurs[1].socket).join(nameRoom);
-    fifoJoueurs.shift(); fifoJoueur.shift();
+    fifoJoueurs.shift();
+    fifoJoueurs.shift();
     console.log('Salon '+room.name+' créé avec '+room.pseudo1+' et '+room.pseudo2+'.');
-    startMatch(room);
+    initMatch(room);
    }
 }
 
@@ -91,6 +117,7 @@ function initMatch(room){
       deck1.push(rows[i]);
     }
   });
+  var deck2 = [];
   connection.query(queryCartesJ2, function(err, rows, fields){
     if (err) throw err;
     console.log('rows = '+rows);
@@ -102,26 +129,21 @@ function initMatch(room){
 
 
   connection.end();
-  var etatMatch = new etatMatch(room.pseudo1, room.pseudo2, deck1, deck2);
-  alert('Match lancé !');
-  console.log(etatMatch);
+  var etatM = new etatMatch(room.pseudo1, room.pseudo2, deck1, deck2);
+  console.log('Match lancé !');
+  console.log(etatM);
 }
 
 var server = http.createServer(function(req,res){
 
   var url_p = url.parse(req.url,true,true);
-
   if(url_p.pathname == '/cli.html'){
     fs.readFile('./cli.html','utf-8',function(error,content){
       res.writeHead(200,{"Content-type":"text/html"});
       res.end(content);
     });
   }
-  else {
-    res.writeHead(404,{"Content-type":"text/html"})
-    res.write("<p><h1>ERREUR 404</h1>Page inexistante</p>");
-    res.end();
-  }
+
 });
 
 var io = require('socket.io').listen(server);
@@ -130,6 +152,7 @@ io.sockets.on('connection', function (socket){
 
   socket.on('chercherMatch',function(pseudo){
       fifoJoueurs.push({socket: socket,pseudo: pseudo});
+      console.log(pseudo+" cherche un match");
       fournirSalons();
   });
 
@@ -137,5 +160,5 @@ io.sockets.on('connection', function (socket){
   //Fonction très très longue :
 });
 
-
 server.listen(8080);
+console.log("server pret");
