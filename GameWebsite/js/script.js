931876a16495db4e8bc820af3d3b16d8db85c291
Vinspi <vinspi13@gmail.com>
2017-03-17 13:29:00 +0100

    ajout fin de partie

--- GameWebsite/js/script.js
+++ GameWebsite/js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     console.log("Document pret");
-    
+
     setZoneSize();
-    
+
     console.log("ScreenW: " + ScreenW);
     console.log("ScreenH: " + ScreenH);
-    
+
     console.log("ZoneW: " + ZoneW);
     console.log("ZoneH: " + ZoneH);
-    
+
     setZoneObjects();
-        
+
     $('#modal_connexion').modal({
       dismissible: false, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
@@ -19,24 +19,25 @@ $(document).ready(function(){
       startingTop: '4%', // Starting top style attribute
       endingTop: '10%', // Ending top style attribute
     });
-    
+
     $('#modal_connexion').modal('open');
-    
-    
+
+
     $("#connexion_btn").click(function(e){
-                
+
         var auth = { 'pseudo': $("#connexion_pseudo").val(), 'mdp': $("#connexion_mdp").val()};
         console.log(auth);
-        socket.emit("AUTH_CLI", auth); 
+        socket.emit("AUTH_CLI", auth);
     });
-    
+
     socket.on("AUTH_CLI_OK", function(obj){
             var $toastContent = $('<span color="red">OK CONNECTE</span>');
             Materialize.toast($toastContent, 6000);
+            Joueur = $('#connexion_pseudo').val();
             $('#modal_connexion').modal('close');
     } );
-    
-    
+
+
     socket.on("AUTH_CLI_FAIL", function(obj){
             var $toastContent = $('<span color="red">Erreur, mauvais pseudo et/ou mot de passe !</span>');
             Materialize.toast($toastContent, 6000);
@@ -44,16 +45,10 @@ $(document).ready(function(){
             $("#connexion_mdp").val("");
 
     } );
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
   });
-
-
-
-
-
-
